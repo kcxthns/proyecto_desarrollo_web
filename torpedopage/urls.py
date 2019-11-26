d177a3038a8c8045b87a448d4adf5e5f01480aa6
kcxthns <illidan19@gmail.com>
2019-11-26 13:54:10 -0300

    cambio visual buscar torpedo

--- torpedopage/urls.py
+++ torpedopage/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', views.login,  name='torpedo_index'),
     path('nosotros', views.nosotros, name='nosotros'),
     path('empezar', views.empezar ,name='empezar'),
-    path('user_page', views.base_user, name='user_page'),
+    path('user_page', views.archivoTorpedo, name='user_page'),
     path('logout', views.logout),
     path('preferencias', views.preferencias, name='preferencias'),
     path('agregartorpedo', views.agregartorpedo, name='agregartorpedo'),
